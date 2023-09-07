2e2acebf1efc858195a3085a5053f39c67d12fbd
Luis <65362007+Luisorozco42@users.noreply.github.com>
2023-09-07 13:39:25 -0600

    Update train.py
    This script is complete, I guess.

--- src/model/train.py
+++ src/model/train.py
@@ -7,13 +7,15 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import mlflow 
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
-
+    #It begins a new execution of MLflow
+    mlflow.start_run()
+    
     # read data
     df = get_csvs_df(args.training_data)
 
@@ -22,7 +24,9 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
+    
+    mlflow.log_params({"reg_rate": args.reg_rate})
+    mlflow.end_run()
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -33,13 +37,21 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    
+    X = df.drop(columns=['Diabetic'])  
+    y = df['Diabetic'] 
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # Ajusta los parámetros según tus necesidades
 
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    
+    accuracy = model.score(X_test, y_test)
+    print(f"Accuracy of the model in the test set: {accuracy}")
 
 def parse_args():
     # setup arg parser
@@ -63,12 +75,12 @@ if __name__ == "__main__":
     print("\n\n")
     print("*" * 60)
 
-    # parse args
-    args = parse_args()
-
-    # run main function
-    main(args)
-
+    with mlflow.start_run():
+        # parse args
+        args = parse_args()
+        
+        # run main function
+        main(args)
     # add space in logs
     print("*" * 60)
     print("\n\n")
